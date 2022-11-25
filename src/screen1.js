7803534f9e572e4594a109cbdae4d1ba79856944
Samir Kaushik <samirkaushik2002@gmail.com>
2022-11-26 03:26:14 +0530

    Basic layout of first screen added

---
+++ src/screen1.js
@@ -0,0 +1,10 @@
+const Screen1 = () => {
+
+    return (<>
+    <h1>First Screen</h1>
+    <div id="int_match">International Matches</div>
+    <div><input type="search" name="search" id="search" placeholder="Search for Matches" /></div>
+    <div className="matches"></div>
+    </>);
+}
+export default Screen1;
