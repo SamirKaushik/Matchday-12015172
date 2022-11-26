b5b3b7c143943011525ba348de7862b4653f4c84
Samir Kaushik <samirkaushik2002@gmail.com>
2022-11-26 16:22:54 +0530

    removed readme

--- src/screen1.js
+++ src/screen1.js
@@ -69,33 +69,29 @@ const Screen1 = () => {
         })
     }, []);
     return (
-    <>
-        <h1>First Screen</h1>
-        <div id="screen1_title">International Matches</div>
-        <div><input type="search" name="search" id="search" placeholder="Search for Matches" /></div>
-        <div className="matches">
-            {() => {
-                for (var i = 0; i <= pageNumber; i++) {
-                    for (var j = 0; j < pages[pageNumber].fixtures.length; j++) {
-                        const x = pages[i].fixtures[j];
-                        const score_array = [];
-                        if (x.a1 !== 0) score_array.append(x.a1 + "-" + x.b1);
-                        if (x.a2 !== 0) score_array.append(", " + x.a2 + "-" + x.b2);
-                        if (x.a3 !== 0) score_array.append(", " + x.a3 + "-" + x.b3);
-                        return <Link to="/screen2">
-                            <Card round={x.round} scores={score_array} player1={x.team1.name} player2={x.team2.name} winner={x.winner} />
-                        </Link>
+        <>
+            <h1>First Screen</h1>
+            <div id="screen1_title">International Matches</div>
+            <div><input type="search" name="search" id="search" placeholder="Search for Matches" /></div>
+            <div className="matches">
+                {() => {
+                    for (var i = 0; i <= pageNumber; i++) {
+                        for (var j = 0; j < pages[pageNumber].fixtures.length; j++) {
+                            const x = pages[i].fixtures[j];
+                            const score_array = [];
+                            if (x.a1 !== 0) score_array.append(x.a1 + "-" + x.b1);
+                            if (x.a2 !== 0) score_array.append(", " + x.a2 + "-" + x.b2);
+                            if (x.a3 !== 0) score_array.append(", " + x.a3 + "-" + x.b3);
+                            return <Link to="/screen2">
+                                <Card round={x.round} scores={score_array} player1={x.team1.name} player2={x.team2.name} winner={x.winner} />
+                            </Link>
+                        }
                     }
-                }
-            }}
-        </div>
-        {() => {
-            if (loading) return <div className="loading">Loading...</div>
-        }}
-        {() => {
-            if (error) return <div className="error">Error</div>
-        }}
-    </>
+                }}
+            </div>
+            {loading && <div className="loading">Loading...</div>}
+            {error && <div className="error">Error</div>}
+        </>
     );
 }
 export default Screen1;
