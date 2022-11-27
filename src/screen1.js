252f21ec93e3233978272d6cc6ce574b366ef9eb
Samir Kaushik <samirkaushik2002@gmail.com>
2022-11-27 12:51:14 +0530

    infinite scroller imported and used

--- src/screen1.js
+++ src/screen1.js
@@ -4,29 +4,14 @@ import { Link } from "react-router-dom";
 import Card from "./card";
 import data from "./data";
 import './screen1.css'
+import InfiniteScroll from "react-infinite-scroll-component"
 const Screen1 = () => {
-    var pageNumber=0;
+    var pageNumber = 0;
     var url = "https://matchday.ai/referee/champ/fixture/dummy-matches?page=0";
     const [pages, setPages] = useState([]);
     const [loadedPages, setLoadedPages] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-
-
-    //getting first page using API but the API isn't working and giving CORS errors so I'm using data from file
-    //I've still written the code
-    // useEffect(() => {
-    //     setLoading(true)
-    //     setError(null)
-    //     fetch("https://matchday.ai/referee/champ/fixture/dummy-matches?page=0/allow-cors", { mode: 'cors', credentials: 'include' }).then(response => {
-    //         if (response.ok)
-    //             return response.json();
-    //         throw response;
-    //     }).then(data => {
-    //         setPages([data]);
-    //         setLoading(false);
-    //     }).catch(error => { setError(error) })
-    // }, []);
 
     //getting first page
     useEffect(() => {
@@ -40,46 +25,12 @@ const Screen1 = () => {
 
     }, []);
 
-    //adding scroll event to make infinite scroller using API
-    // useEffect(() => {
-    //     window.addEventListener('scroll', () => {
-    //         if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
-    //             if (pageNumber !== 0 && pageNumber < 9) {
-    //                 setLoading(true)
-    //                 setError(null)
-    //                 setPageNumber((num) => num + 1)
-    //                 url = "https://matchday.ai/referee/champ/fixture/dummy-matches?page=" + toString(pageNumber)
-    //                 fetch(url + "/allow-cors", { mode: 'cors', credentials: 'include' }).then(response => {
-    //                     if (response.ok)
-    //                         return response.json();
-    //                     throw response;
-    //                 }).then(data => {
-    //                     setPages([...pages, data]);
-    //                     setLoading(false);
-    //                 }).catch(error => { setError(error) })
-    //             }
-    //         }
-    //     })
-    // }, []);
-
-    //code for infinite scroller
-    useEffect(() => {
-        window.addEventListener('scroll', () => {
-            const offsetHeight = document.documentElement.offsetHeight;
-            const innerHeight = window.innerHeight;
-            const scrollTop = document.documentElement.scrollTop;
-            const hasReachedBottom = offsetHeight - (innerHeight + scrollTop) <= 10;
-            if (hasReachedBottom) {
-                     setTimeout(() => {
-                        pageNumber++;
-                        if(pageNumber<=9)
-                        setPages(pages.concat(data[pageNumber]));
-                        setLoadedPages(pages);
-                    }, 1000);
-
-            }
-        })
-    }, []);
+    const fetchData = async(num) => {
+        setTimeout(() => {
+         const newPages=data[num];
+         setPages([...pages,newPages]);
+        }, 500);
+      };
 
     // searching and filtering
     const filter = (value) => {
@@ -93,45 +44,37 @@ const Screen1 = () => {
         searchbar.addEventListener('input', () => {
             filter(searchbar.value);
         })
+
+    
     return (
         <>
             <h1>First Screen</h1>
             <div style={{ padding: "0px 20px" }}>
                 <div id="screen1_title">International Matches</div>
                 <div style={{ display: "flex", justifyContent: "flex-end" }}><input type="search" name="search" id="search" placeholder="Search for Matches" /></div>
-                <div className="matches">
-                    {/* {() => {
-                    for (var i = 0; i <= pageNumber; i++) {
-                        for (var j = 0; j < pages[pageNumber].fixtures.length; j++) {
-                            const x = pages[i].fixtures[j];
-                            const score_array = [];
-                            if (x.a1 !== 0) score_array.append(x.a1 + "-" + x.b1);
-                            if (x.a2 !== 0) score_array.append(", " + x.a2 + "-" + x.b2);
-                            if (x.a3 !== 0) score_array.append(", " + x.a3 + "-" + x.b3);
-                            return <Link to="/screen2">
-                                <Card round={x.round} scores={score_array} player1={x.team1.name} player2={x.team2.name} winner={x.winner} />
-                            </Link>
-                        }
-                    }
-                }} */}
-
-                    {
-                        pages.map((page) => {
-                            return page.fixtures.map((x) => {
-                                const score_array = [];
-                                if (x.a1 !== 0) score_array.push(x.a1 + "-" + x.b1);
-                                if (x.a2 !== 0) score_array.push(", " + x.a2 + "-" + x.b2);
-                                if (x.a3 !== 0) score_array.push(", " + x.a3 + "-" + x.b3);
-                                return <Link to="/screen2" key={x._id} style={{ textDecoration: 'none' }}>
-                                    <Card round={x.round} scores={score_array} player1={x.team1[0].name} player2={x.team2[0].name} winner={x.winner} />
-                                </Link>
+                    <InfiniteScroll
+                        className="matches"
+                        dataLength={pages.length}
+                        next={()=>fetchData(pages.length)}
+                        hasMore={true||pages[pages.length-1].hasMorePage}
+                    >
+                        {
+                            pages.map((page) => {
+                                if(page!==undefined)
+                                return page.fixtures.map((x) => {
+                                    const score_array = [];
+                                    if (x.a1 !== 0) score_array.push(x.a1 + "-" + x.b1);
+                                    if (x.a2 !== 0) score_array.push(", " + x.a2 + "-" + x.b2);
+                                    if (x.a3 !== 0) score_array.push(", " + x.a3 + "-" + x.b3);
+                                    return <Link to="/screen2" key={x._id} style={{ textDecoration: 'none' }}>
+                                        <Card round={x.round} scores={score_array} player1={x.team1[0].name} player2={x.team2[0].name} winner={x.winner} />
+                                    </Link>
+                                })
                             })
-                        })
-                    }
+                        }
+                    </InfiniteScroll>
                 </div>
-                {loading && <div className="loading">Loading...</div>}
-                {error && <div className="error">Error</div>}
-            </div>
+            
         </>
     );
 }
