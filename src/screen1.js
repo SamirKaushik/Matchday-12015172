85e5677b5b13ff9f1a7df2c8ec17b5a10b4923d9
Samir Kaushik <samirkaushik2002@gmail.com>
2022-11-27 16:52:51 +0530

    search functionality added

--- src/screen1.js
+++ src/screen1.js
@@ -8,7 +8,7 @@ import InfiniteScroll from "react-infinite-scroll-component"
 const Screen1 = () => {
     // var url = "https://matchday.ai/referee/champ/fixture/dummy-matches?page=0"; this has been used to get all the data and stored itcin data.js file
     const [pages, setPages] = useState([]);
-    const [loadedPages,setLoadedPages]=useState([]);
+    const [loadedPages, setLoadedPages] = useState([]);
     // const [loading, setLoading] = useState(true);
     // const [error, setError] = useState(null);
     //getting first page using API but the API isn't working and giving CORS errors so I'm using data from file
@@ -30,50 +30,70 @@ const Screen1 = () => {
     useEffect(() => {
         setTimeout(() => {
             setPages([data[0]]);
-            setLoadedPages(data[0]);
+            console.log(pages)
+            setLoadedPages([data[0]]);
+
         }
             , 500);
 
     }, []);
 
-    const fetchData = async(num) => {
+    const fetchData = async (num) => {
         setTimeout(() => {
-         const newPages=data[num];
-         setPages([...pages,newPages]);
-         setLoadedPages([...pages,newPages]);
+            const newPages = data[num];
+            setPages([...pages, newPages]);
+            setLoadedPages([...pages, newPages]);
         }, 500);
-      };
+    };
+
 
     // searching and filtering
     const filter = (value) => {
-        // if(value==="") setPages(loadedPages);
-        // const newPages=loadedPages.filter((page)=>page.fixtures.team1[0].name.toLowerCase().includes(value.toLowerCase())||page.fixtures.team2[0].name.toLowerCase().includes(value.toLowerCase())||page.fixtures.tournament[0].name.toLowerCase().includes(value.toLowerCase()));
-        // setPages(newPages);
         console.log(value);
+        if (value === "") {
+            console.log(loadedPages)
+            return setPages(loadedPages);
+            console.log(pages)
+        }
+
+    
+
+        const newPages = loadedPages.map(
+            (page) => ({
+                ...page, fixtures: page.fixtures.filter(
+                    (card) => card.team1[0].name.toLowerCase().includes(value.toLowerCase())
+                    ||card.team2[0].name.toLowerCase().includes(value.toLowerCase()) || card.tournament[0].name.toLowerCase().includes(value.toLowerCase())
+
+                )
+            })
+        );
+        setPages(newPages);
+        console.log(loadedPages);
     }
+
     const searchbar = document.getElementById("search");
     if (searchbar)
         searchbar.addEventListener('input', () => {
             filter(searchbar.value);
         })
 
-    
+
     return (
         <>
             <h1>First Screen</h1>
             <div style={{ padding: "0px 20px" }}>
                 <div id="screen1_title">International Matches</div>
                 <div style={{ display: "flex", justifyContent: "flex-end" }}><input type="search" name="search" id="search" placeholder="Search for Matches" /></div>
-                    <InfiniteScroll
-                        className="matches"
-                        dataLength={pages.length}
-                        next={()=>fetchData(pages.length)}
-                        hasMore={true||pages[pages.length-1].hasMorePage}
-                    >
-                        {
-                            pages.map((page) => {
-                                if(page!==undefined)
-                                return page.fixtures.map((x) => {
+                <InfiniteScroll
+                    className="matches"
+                    dataLength={pages.length}
+                    next={() => fetchData(pages.length)}
+                    hasMore={pages.length < 9 || pages[pages.length - 1].hasMorePage}
+                >
+                    {
+                        pages.map((page) => {
+                            if (page)
+                                return page.fixtures && page.fixtures.map((x) => {
                                     const score_array = [];
                                     if (x.a1 !== 0) score_array.push(x.a1 + "-" + x.b1);
                                     if (x.a2 !== 0) score_array.push(", " + x.a2 + "-" + x.b2);
@@ -82,12 +102,12 @@ const Screen1 = () => {
                                         <Card round={x.round} scores={score_array} player1={x.team1[0].name} player2={x.team2[0].name} winner={x.winner} />
                                     </Link>
                                 })
-                                else return "";
-                            })
-                        }
-                    </InfiniteScroll>
-                </div>
-            
+                            else return "";
+                        })
+                    }
+                </InfiniteScroll>
+            </div>
+
         </>
     );
 }
--- src/screen2.js
+++ src/screen2.js
@@ -10,6 +10,7 @@ const Screen2 = () => {
         <div id="video">
             <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/HucIqi8Lw3E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
+        {/* vo page dikha screen1.js */}
 
     </>);
 }
