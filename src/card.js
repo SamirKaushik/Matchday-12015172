1b5a1ed86fafc171e478d96a67a8e789240b8046
Samir Kaushik <samirkaushik2002@gmail.com>
2022-11-30 02:40:47 +0530

    res added

--- src/card.js
+++ src/card.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import "./card.css"
 const flag = "https://upload.wikimedia.org/wikipedia/commons/4/41/Flag_of_India.svg";
-const logo = "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/da00ca61-beb4-4735-8ff8-f6cff8597fc3/logo_white.png";
-const crown = "https://s3-us-west-2.amazonaws.com/secure.notion-static.com/08f830ae-ce65-42f5-b243-b7a3f8a15b5f/crown.png";
+const logo = "./res/logo_white.png";
+const crown = "./res/crown.png";
 
 const Card = ({ round, scores, player1, player2, winner, tournament }) => {
     return (<>
